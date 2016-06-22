fb78d9ff9825fbad5915895c8afbdea43cfef918
Marcelo Romulo Fernandes <persapiens@gmail.com>
2016-06-21 21:53:29 -0300

    convert PrimeFacesLayout to PrimeFacesLayoutJSBS project using jsf-myfaces-spring-boot-starter

---
+++ PrimeFacesLayoutJSBS/src/main/java/de/beyondjava/bootsfaces/examples/JsfSpringBootStarterExampleApplication.java
@@ -0,0 +1,13 @@
+package de.beyondjava.bootsfaces.examples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsfSpringBootStarterExampleApplication {
+    
+	public static void main(String[] args) {
+		SpringApplication.run(JsfSpringBootStarterExampleApplication.class, args);
+	}
+    
+}
